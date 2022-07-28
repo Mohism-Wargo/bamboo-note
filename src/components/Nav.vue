df367d6bd1eb90b60f1c34dfc3d0d6de1659a6e0
Mohism <hk81206676@163.com>
2022-07-28 16:28:08 +0800

    导航栏样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,43 +1,48 @@
 <template>
-    <div class="nav">
-        <router-link to="/money">
-            <svg>
-                <use xlink:href="#money"/>
-            </svg>
+    <nav>
+        <router-link to="/money" class="item"> 
+            <Icon name="money" />
             记账
         </router-link>
-        |
-        <router-link to="/labels">
-            <svg>
-                <use xlink:href="#labels"/>
-            </svg>
+
+        <router-link to="/labels" class="item">
+            <Icon name="labels" />
             标签
         </router-link>
-        |
-        <router-link to="/statistics">
-            <svg>
-                <use xlink:href="#statistics"/>
-            </svg>
+
+        <router-link to="/statistics" class="item">
+            <Icon name="money" />
             统计
         </router-link>
-     </div>
+    </nav>
 </template>
 
 <script lang="ts">
-
-   let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-   try{
-    importAll(require.context('../assets/icons/', true, /\svg$/));
-   } catch(error){
-       console.log(error);
-   };
-
+import Icon from "./Icon.vue";
 
    export default{
-    name:'Nav'
-   };
+    name: "Nav",
+    components: { Icon }
+};
 </script>
 
 <style lang="scss" scoped>
-    .nav{border: 1px solid red;}
+    nav {
+        display:flex;
+        flex-direction: row;
+        box-shadow: 0 0 3px rgba(0,0,0,0.25);
+        font-size: 13px;
+        > .item {
+            padding: 2px 0;
+            width: 33.33333333%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            .icon{
+                width: 32px;
+                height: 32px;
+            }
+        }
+    }
 </style>
