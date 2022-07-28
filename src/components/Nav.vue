1df1a908a0d7e01df287209cdc5ddf2ddacfe770
Mohism <hk81206676@163.com>
2022-07-28 16:47:02 +0800

    更新viewport

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
     <nav>
-        <router-link to="/money" class="item"> 
+        <router-link to="/money" class="item" active-class="selected"> 
             <Icon name="money" />
             记账
         </router-link>
 
-        <router-link to="/labels" class="item">
+        <router-link to="/labels" class="item" active-class="selected">
             <Icon name="labels" />
             标签
         </router-link>
 
-        <router-link to="/statistics" class="item">
+        <router-link to="/statistics" class="item" active-class="selected">
             <Icon name="money" />
             统计
         </router-link>
@@ -43,6 +43,9 @@ import Icon from "./Icon.vue";
                 width: 32px;
                 height: 32px;
             }
+        }
+        > .item.selected{
+            background-color: rgb(197, 235, 235);
         }
     }
 </style>
