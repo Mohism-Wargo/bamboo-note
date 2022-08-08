e47b43c8ed9f91b0cd998bb290606b2ead146d39
Mohism <hk81206676@163.com>
2022-08-08 14:08:27 +0800

    svg颜色

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,30 @@
 <template>
     <div>
         <Layout>
-            Money.vue
+            <div class="tags">
+                <ul class="current">
+                    <li>衣</li>
+                    <li>食</li>
+                    <li>住</li>
+                    <li>行</li>
+                </ul>
+                <div class="new">
+                    <button>新增标签</button>
+                </div>
+            </div>
+            <div>
+                <label class="notes">
+                    <span class="name">备注</span> 
+                    <input type="text">
+                </label>
+            </div>
+            <div>
+                <ul class="types">
+                    <li class="selected">支出</li>
+                    <li>收入</li>
+                </ul>
+            </div>
+            <div>number pad</div>
         </Layout>
     </div>
 </template>
