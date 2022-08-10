a1c59f09b570a375f13eac648730ca7cefd08c96
Mohism <hk81206676@163.com>
2022-08-10 13:19:14 +0800

    基本完成NumberPad组件

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -2,20 +2,20 @@
     <div class="numberPad">
         <div class="output">{{output}}</div>
         <div class="buttons">
-            <button @click=inputContent>1</button>
-            <button @click=inputContent>2</button>
-            <button @click=inputContent>3</button>
-            <button>删除</button>
-            <button @click=inputContent>4</button>
-            <button @click=inputContent>5</button>
-            <button @click=inputContent>6</button>
-            <button>清空</button>
-            <button @click=inputContent>7</button>
-            <button @click=inputContent>8</button>
-            <button @click=inputContent>9</button>
-            <button class="ok">确定</button>
-            <button class="zero" @click=inputContent>0</button>
-            <button @click=inputContent>.</button>
+            <button @click="inputContent">1</button>
+            <button @click="inputContent">2</button>
+            <button @click="inputContent">3</button>
+            <button @click="remove">删除</button>
+            <button @click="inputContent">4</button>
+            <button @click="inputContent">5</button>
+            <button @click="inputContent">6</button>
+            <button @click="clear">清空</button>
+            <button @click="inputContent">7</button>
+            <button @click="inputContent">8</button>
+            <button @click="inputContent">9</button>
+            <button @click="ok" class="ok">确定</button>
+            <button class="zero" @click="inputContent">0</button>
+            <button @click="inputContent">.</button>
         </div>
     </div>
 </template>
@@ -32,16 +32,29 @@ export default class NumberPad extends Vue {
         const button = (event.target as HTMLButtonElement);
         const input = button.textContent!;
         if(this.output.length === 16) {return;}
-        if('0123456789'.indexOf(input) >= 0){
-            this.output = input;
-        } else{
-            this.output += input;
+        if(this.output === '0'){
+          if('0123456789'.indexOf(input) >= 0){
+              this.output = input;
+          } else{
+              this.output += input;
+          }
+          return;
         }
-        return;
+        if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+            this.output += input;
       }
-      if (this.output.indexOf('.') >= 0 && input === '.') {return;}
-      this.output += input;
-}
+      remove(){
+        if (this.output.length === 1){
+            this.output = '0';
+        } else{
+            this.output = this.output.slice(0,-1);
+        }        
+      }
+      clear(){
+        this.output = '0';
+      }
+      ok(){} 
+    }
 </script>
 
 <style lang="scss" scoped>
